6c061eb03df7ecdbb1afb234ccb00926b3d8ac43
V <2393630@cognizant.com>
2026-02-12 11:14:55 +0530

    unit test coverage update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,10 +7,10 @@ import biomass from "../assets/Biomass_Energy.jpg";
 import ocean from "../assets/Ocean_Energy.jpg";
 import geothermal from "../assets/geothermal.jpg";
 import coal from "../assets/Coal.jpg";
-import oil from "../assets/oil.jpg";
-import gas from "../assets/gas.jpg";
+import oil from "../assets/Oil.jpg";
+import gas from "../assets/Gas.jpg";
 import nuclear from "../assets/Nuclear_Energy.jpg";
-import uranium from "../assets/uranium.jpg";
+import uranium from "../assets/Uranium.jpg";
 import minerals from "../assets/Minerals.jpg";
 
 const Projects = () => {
