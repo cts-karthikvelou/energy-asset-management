620be91d98cf8147d41b50cb27cb1560e5ca04cb
V <2393630@cognizant.com>
2026-02-17 12:13:34 +0530

    sonarqube bug fixing

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-// import { FaGoogle, FaFacebook } from "react-icons/fa";
 
 const Signup = () => {
   const navigate = useNavigate();
